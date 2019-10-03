49c6a64214ef410efd28df8a4c473b930dfd0a9d
Michael Andrés <mamurciac@unal.edu.co>
2019-10-02 20:40:16 -0500

    Example 0: Hello World

---
+++ Example0_HelloWorld/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+/* This exercise consists of looking a simple message, is to say, a Hello World program. */
+
+int main(){
+    cout << "This is the first program." << endl;
+    cout << "Welcome to Programming and Numerical Methods course." << endl;
+    return 0;
+}
